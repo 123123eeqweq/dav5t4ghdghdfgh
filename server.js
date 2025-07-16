3ad8e404718c53076d978b6b16d982b86b01efdd
Твой Имя <@email.com>
2025-07-16 03:54:19 +0300

    first commit

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ require('dotenv').config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:5173' }));
+app.use(cors({
+  origin: [
+    'https://binary-murex.vercel.app',
+    'http://localhost:5173'
+  ],
+  credentials: true
+}));
 
 // Подключение к MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
